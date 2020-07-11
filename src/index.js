adbba42e09e9efb2d8ec842333cf609c802f9b04
EmirVildanov <reddog201030@gmail.com>
2020-07-12 00:20:27 +0300

    Moved out tasks info into context and added some css

--- src/index.js
+++ src/index.js
@@ -1,63 +1,77 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const TasksContext = React.createContext()
+
 class Task extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state.text = props.text;
-    }
-
-    state = {
-        checked: false,
-        text: '',
-    }
-
-    changeReadyStatus() {
-        if (!this.state.checked) {
-            this.state.checked = !this.state.checked
-            return "checked"
-        }
-        this.state.checked = !this.state.checked
-        return ""
-    }
-
     render() {
         return (
             <div>
-                <span
-                    className={this.state.checked ? "checked" : ""}
-                    onClick={() => this.setState({checked: !this.state.checked})}>
-                    <TasksContext.Consumer>
-                        {context => (
-                            <button className="close" onClick={() => context.deleteTask(this.state.text)}>Remove</button>
-                        )}
-                    </TasksContext.Consumer>
-                    {this.state.text}
-                </span>
+                <TasksContext.Consumer>
+                    {context => (
+                        <span
+                            className={this.props.checked ? "checked" : ""}
+                            onClick={() => context.changeStatus(this.props.text)}
+                        >
+                            <button className="close"
+                                    onClick={(event) => {
+                                        context.deleteTask(this.props.text);
+                                        event.stopPropagation();
+                                    }}
+                            >
+                                Remove
+                            </button>
+                            {this.props.text}
+                        </span>
+                    )}
+                </TasksContext.Consumer>
             </div>
         )
     }
 }
 
-const TasksContext = React.createContext()
-
 class TasksProvider extends React.Component {
     state = {
-        tasks: Array(),
+        tasks: [],
     }
 
     addTaskToArray(text) {
-        const newArray = this.state.tasks.slice()
-        newArray.push(<Task text={text} />)
-        return newArray
+        for (let i = 0; i < this.state.tasks.length; ++i) {
+            if (this.state.tasks[i].text === text) {
+                alert("Current task is already in the table");
+                return this.state.tasks;
+            }
+        }
+        const newArray = this.state.tasks.slice();
+        newArray.push(
+            {
+                text: text,
+                checked: false
+            });
+        return newArray;
     }
 
-    deleteTaskFromArray(task) {
-        const newArray = this.state.tasks.slice()
-        const indexOfDeletingElement = this.state.tasks.indexOf(task)
-        newArray.splice(indexOfDeletingElement, 1)
-        return newArray
+    findCurrentTaskIndex(taskText) {
+        for (let i = 0; i < this.state.tasks.length; ++i) {
+            if (this.state.tasks[i].text === taskText) {
+                return i;
+            }
+        }
+    }
+
+    deleteTaskFromArray(text) {
+        const newArray = this.state.tasks.slice();
+        const indexOfDeletingElement = this.findCurrentTaskIndex(text);
+        newArray.splice(indexOfDeletingElement, 1);
+        return newArray;
+    }
+
+    changeTaskReadyStatus(text) {
+        const newArray = this.state.tasks.slice();
+        const indexOfDeletingElement = this.findCurrentTaskIndex(text)
+        newArray[indexOfDeletingElement].checked = !newArray[indexOfDeletingElement].checked;
+        return newArray;
     }
 
     render() {
@@ -65,7 +79,8 @@ class TasksProvider extends React.Component {
             <TasksContext.Provider value={{
                 state: this.state,
                 addTask: (task) => this.setState({tasks: this.addTaskToArray(task)}),
-                deleteTask: (task) => this.setState({tasks: this.deleteTaskFromArray(task)})
+                deleteTask: (task) => this.setState({tasks: this.deleteTaskFromArray(task)}),
+                changeStatus: (task) => this.setState( {tasks: this.changeTaskReadyStatus(task)}),
             }}>
                 {this.props.children}
             </TasksContext.Provider>
@@ -73,64 +88,67 @@ class TasksProvider extends React.Component {
     }
 }
 
-class TasksBoard extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <TasksContext.Consumer>
-                    {context => (
-                        context.state.tasks
-                    )}
-                </TasksContext.Consumer>
-            </React.Fragment>
-        )
-    }
+function TasksBoard() {
+    return (
+        <React.Fragment>
+            <TasksContext.Consumer>
+                {context => (
+                    context.state.tasks.map(element => <Task key={element.text} checked={element.checked} text={element.text}/>)
+                )}
+            </TasksContext.Consumer>
+        </React.Fragment>
+    )
 }
 
 class InputPane extends React.Component {
+    confirmInput(context) {
+        const input = document.getElementById("newTaskInput").value
+        if (input !== '') {
+            context.addTask(input);
+            document.getElementById("newTaskInput").value = "";
+        } else {
+            alert('Please enter something');
+        }
+    }
+
     render() {
         return (
-            <TasksContext.Consumer>
-                {context => (
-                    <div className="header">
-                        <h2 style={ { margin: 5 } }>My To Do List</h2>
-                        <input
-                            type="text"
-                            id="newTaskInput"
-                            placeholder="Title..."
-                            onKeyDown={event => {
-                                if (event.keyCode === 13) {
-                                    context.addTask(document.getElementById("newTaskInput").value);
-                                    document.getElementById("newTaskInput").value = "";
-                                }
-                            }}
-                        >
-                        </input>
-                        <span
-                            onClick={() => {
-                                context.addTask(document.getElementById("newTaskInput").value);
-                                document.getElementById("newTaskInput").value = "";}
-                            }
-                            className="addBtn">Add
-                        </span>
-                    </div>
-                )}
-            </TasksContext.Consumer>
+            <div className="header">
+                <h2 style={{margin: 5}}>My To Do List</h2>
+                <TasksContext.Consumer>
+                    {context => (
+                        <React.Fragment>
+                            <input
+                                type="text"
+                                id="newTaskInput"
+                                placeholder="Enter new task..."
+                                onKeyDown={event => {
+                                    event.keyCode === 13 && this.confirmInput(context)
+                                }}
+                            >
+                            </input>
+                            <span onClick={() => {
+                                this.confirmInput(context)
+                            }} className="addBtn">
+                                Add
+                            </span>
+                        </React.Fragment>
+                    )}
+                </TasksContext.Consumer>
+            </div>
         )
     }
 }
 
-class ToDoApp extends React.Component {
-    render() {
-        return (
-            <TasksProvider>
-                <div>
-                    <InputPane />
-                    <TasksBoard />
-                </div>
-            </TasksProvider>
-        );
-    }
+function ToDoApp() {
+    return (
+        <TasksProvider>
+            <div>
+                <InputPane/>
+                <TasksBoard/>
+            </div>
+        </TasksProvider>
+    );
 }
 
 ReactDOM.render(
